9f45ae87b5be77d43f008c048f03084ad8209864
Davi de Almeida <daviallmeida77@gmail.com>
2026-04-25 11:34:58 -0300

    ex_idade.java para comparação de idades em java

---
+++ ex_idade.java
@@ -0,0 +1,10 @@
+public class ex_idade
+{
+	public static void main(String[] args) {
+    int minhaIdade = 15;
+    int idadeProf = 21;
+    boolean souMaisNovo = minhaIdade < idadeProf;
+    System.out.println(souMaisNovo);
+	}
+	
+}
